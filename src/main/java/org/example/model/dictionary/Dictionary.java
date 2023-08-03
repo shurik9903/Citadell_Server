7b20768e0102fd50c5d31e057c55765ea4d8516e
shurik9903 <59256714+shurik9903@users.noreply.github.com>
2023-08-03 17:43:33 +0700

    add dictionary model

--- src/main/java/org/example/model/dictionary/Dictionary.java
+++ src/main/java/org/example/model/dictionary/Dictionary.java
@@ -4,39 +4,235 @@ import jakarta.inject.Inject;
 import jakarta.json.bind.Jsonb;
 import jakarta.json.bind.JsonbBuilder;
 import jakarta.ws.rs.core.Response;
-import org.example.controller.WebSocket.WebSocket;
-import org.example.model.database.IDataBaseWork;
+import org.example.data.entity.ESimpleWord;
+import org.example.data.entity.ESpellingVariants;
+import org.example.data.entity.ETypeWord;
+import org.example.data.mydata.DWord;
+import org.example.model.database.dictionaryWork.IDBDictionaryWork;
 
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Dictionary implements IDictionary {
     @Inject
-    private IDataBaseWork DataBaseWork;
+    private IDBDictionaryWork dataBase;
 
     @Override
-    public Response loadWord(String word){
+    public Response loadSimpleWord(String wordID){
         Jsonb jsonb = JsonbBuilder.create();
         Map<String, String> Result = new HashMap<>();
 
-
         try {
-            if (!DataBaseWork.ping()) {
-                Result.put("Msg", "Нет соединения с базой данных");
-                return Response.ok(jsonb.toJson(Result)).build();
-            }
+            dataBase.ping();
 
+            ESimpleWord eSimpleWord = dataBase.loadSimpleWord(wordID);
+            if (eSimpleWord == null)
+                throw new Exception("Данное слово не найдено");
 
+            Result.put("word", eSimpleWord.getWord());
 
-            Result.put("Msg", "");
             return Response.ok(jsonb.toJson(Result)).build();
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
-            return Response.status(Response.Status.BAD_REQUEST).entity("|Ошибка: " + e.getMessage()).build();
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
         }
 
+    }
+
+    @Override
+    public Response loadSimpleWords() {
+        Jsonb jsonb = JsonbBuilder.create();
+        Map<String, String> Result = new HashMap<>();
+
+        try {
+            dataBase.ping();
+
+            ArrayList<ESimpleWord> eSimpleWords = dataBase.loadSimpleWords();
+
+            Result.put("words", jsonb.toJson(eSimpleWords));
+
+            return Response.ok(jsonb.toJson(Result)).build();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+    }
+
+    @Override
+    public Response saveSimpleWord(String word) {
+        Jsonb jsonb = JsonbBuilder.create();
+
+        try {
+            dataBase.ping();
+
+            DWord dWord =  jsonb.fromJson(word, DWord.class);
+
+            dataBase.saveSimpleWord(dWord.getWord(), dWord.getTypeID());
+
+            return Response.ok(jsonb.toJson("ok")).build();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+    }
+
+    @Override
+    public Response updateSimpleWord(String word) {
+        Jsonb jsonb = JsonbBuilder.create();
+
+        try {
+            dataBase.ping();
+
+            DWord dWord =  jsonb.fromJson(word, DWord.class);
+
+            dataBase.updateSimpleWord(dWord.getId(), dWord.getWord(), dWord.getTypeID());
+
+            return Response.ok(jsonb.toJson("ok")).build();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+    }
+
+    @Override
+    public Response deleteSimpleWord(int wordID){
+        Jsonb jsonb = JsonbBuilder.create();
+
+        try {
+            dataBase.ping();
+
+            dataBase.deleteSimpleWord(wordID);
+
+            return Response.ok(jsonb.toJson("ok")).build();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+    }
+
+    @Override
+    public Response loadSpellingWord(String wordID){
+        Jsonb jsonb = JsonbBuilder.create();
+        Map<String, String> Result = new HashMap<>();
+
+        try {
+            dataBase.ping();
+
+            ESpellingVariants eSpellingVariants = dataBase.loadSpellingWord(wordID);
+            if (eSpellingVariants == null)
+                throw new Exception("Данное слово не найдено");
+
+            Result.put("word", eSpellingVariants.getWord());
+
+            return Response.ok(jsonb.toJson(Result)).build();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+
+    }
+
+    @Override
+    public Response loadSpellingWords() {
+        Jsonb jsonb = JsonbBuilder.create();
+        Map<String, String> Result = new HashMap<>();
+
+        try {
+            dataBase.ping();
+
+            ArrayList<ESpellingVariants> eSpellingVariants = dataBase.loadSpellingWords();
+
+            Result.put("words", jsonb.toJson(eSpellingVariants));
+
+            return Response.ok(jsonb.toJson(Result)).build();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+    }
+
+    @Override
+    public Response saveSpellingWord(String word) {
+        Jsonb jsonb = JsonbBuilder.create();
+
+        try {
+            dataBase.ping();
+
+            DWord dWord =  jsonb.fromJson(word, DWord.class);
+
+            dataBase.saveSpellingWord(dWord.getWord(), dWord.getSimpleID());
+
+            return Response.ok(jsonb.toJson("ok")).build();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+    }
+
+    @Override
+    public Response loadTypeWords() {
+        Jsonb jsonb = JsonbBuilder.create();
+        Map<String, String> Result = new HashMap<>();
+
+        try {
+            dataBase.ping();
+
+            ArrayList<ETypeWord> eTypeWords = dataBase.loadTypeWords();
+
+            Result.put("words", jsonb.toJson(eTypeWords));
+
+            return Response.ok(jsonb.toJson(Result)).build();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+    }
+
+    @Override
+    public Response updateSpellingWord(String word) {
+        Jsonb jsonb = JsonbBuilder.create();
+
+        try {
+            dataBase.ping();
+
+            DWord dWord =  jsonb.fromJson(word, DWord.class);
+
+            dataBase.updateSpellingWord(dWord.getId(), dWord.getWord(), dWord.getSimpleID());
+
+            return Response.ok(jsonb.toJson("ok")).build();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+    }
+
+    @Override
+    public Response deleteSpellingWord(int wordID){
+        Jsonb jsonb = JsonbBuilder.create();
+
+        try {
+            dataBase.ping();
+
+            dataBase.deleteSpellingWord(wordID);
+
+            return Response.ok(jsonb.toJson("ok")).build();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
     }
 
 }
--- src/main/java/org/example/model/dictionary/IDictionary.java
+++ src/main/java/org/example/model/dictionary/IDictionary.java
@@ -3,5 +3,26 @@ package org.example.model.dictionary;
 import jakarta.ws.rs.core.Response;
 
 public interface IDictionary {
-    Response loadWord(String word);
+
+    Response loadSimpleWord(String wordID);
+
+    Response saveSimpleWord(String word);
+
+    Response deleteSimpleWord(int wordID);
+
+    Response loadSpellingWord(String wordID);
+
+    Response loadSimpleWords();
+
+    Response loadSpellingWords();
+
+    Response saveSpellingWord(String word);
+
+    Response loadTypeWords();
+
+    Response updateSimpleWord(String word);
+
+    Response updateSpellingWord(String word);
+
+    Response deleteSpellingWord(int wordID);
 }
