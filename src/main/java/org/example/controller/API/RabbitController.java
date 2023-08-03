b854400e7ad2ab742b871a67ee73b87bdd4bff23
shurik9903 <59256714+shurik9903@users.noreply.github.com>
2023-08-03 17:38:34 +0700

    disable rabbit api

--- src/main/java/org/example/controller/API/RabbitController.java
+++ src/main/java/org/example/controller/API/RabbitController.java
@@ -1,51 +1,51 @@
-package org.example.controller.API;
-
-import jakarta.inject.Inject;
-import jakarta.ws.rs.*;
-import jakarta.ws.rs.core.Response;
-import org.example.model.messageBroker.IMessBrok;
-
-@Path("/test")
-public class RabbitController {
-
-    @Inject
-    private IMessBrok messBrok;
-
-    //Подписаться
-    @GET
-    @Produces("application/json; charset=UTF-8")
-    public Response doGet() {
-        try {
-            return messBrok.consume();
-        } catch (Exception e) {
-            System.out.println("|Ошибка: " + e);
-            return Response.status(Response.Status.BAD_REQUEST).entity("|Ошибка: " + e.getMessage()).build();
-        }
-    }
-
-    //Объявить
-    @POST
-    @Consumes("application/json")
-    @Produces("application/json; charset=UTF-8")
-    public Response doPost(String json) {
-        try {
-            return messBrok.declare(json);
-        } catch (Exception e) {
-            System.out.println("|Ошибка: " + e);
-            return Response.status(Response.Status.BAD_REQUEST).entity("|Ошибка: " + e.getMessage()).build();
-        }
-    }
-
-    //Отправить сообщение
-    @PUT
-    @Produces("application/json; charset=UTF-8")
-    public Response doPut(String json, @HeaderParam("Token") String UserToken) {
-        try {
-            return messBrok.send(json);
-        } catch (Exception e) {
-            System.out.println("|Ошибка: " + e);
-            return Response.status(Response.Status.BAD_REQUEST).entity("|Ошибка: " + e.getMessage()).build();
-        }
-    }
-}
+//package org.example.controller.API;
+//
+//import jakarta.inject.Inject;
+//import jakarta.ws.rs.*;
+//import jakarta.ws.rs.core.Response;
+//import org.example.model.messageBroker.IMessBrok;
+//
+//@Path("/test")
+//public class RabbitController {
+//
+//    @Inject
+//    private IMessBrok messBrok;
+//
+//    //Подписаться
+//    @GET
+//    @Produces("application/json; charset=UTF-8")
+//    public Response doGet() {
+//        try {
+//            return messBrok.consume();
+//        } catch (Exception e) {
+//            System.out.println("|Ошибка: " + e);
+//            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+//        }
+//    }
+//
+//    //Объявить
+//    @POST
+//    @Consumes("application/json")
+//    @Produces("application/json; charset=UTF-8")
+//    public Response doPost(String json) {
+//        try {
+//            return messBrok.declare(json);
+//        } catch (Exception e) {
+//            System.out.println("|Ошибка: " + e);
+//            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+//        }
+//    }
+//
+//    //Отправить сообщение
+//    @PUT
+//    @Produces("application/json; charset=UTF-8")
+//    public Response doPut(String json, @HeaderParam("Token") String UserToken) {
+//        try {
+//            return messBrok.send(json);
+//        } catch (Exception e) {
+//            System.out.println("|Ошибка: " + e);
+//            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+//        }
+//    }
+//}
 
