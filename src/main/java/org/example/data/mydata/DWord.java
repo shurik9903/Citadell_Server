880c387dcffef887c0213498089f6a8408ff4ab6
shurik9903 <59256714+shurik9903@users.noreply.github.com>
2023-08-03 17:39:50 +0700

    word data dictionary

---
+++ src/main/java/org/example/data/mydata/DWord.java
@@ -0,0 +1,42 @@
+package org.example.data.mydata;
+
+public class DWord {
+
+    private Integer id;
+    private String word;
+    private Integer typeID;
+    private Integer simpleID;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public Integer getTypeID() {
+        return typeID;
+    }
+
+    public void setTypeID(Integer typeID) {
+        this.typeID = typeID;
+    }
+
+    public Integer getSimpleID() {
+        return simpleID;
+    }
+
+    public void setSimpleID(Integer simpleID) {
+        this.simpleID = simpleID;
+    }
+
+}
