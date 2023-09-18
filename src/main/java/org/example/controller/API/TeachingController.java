e92fd9dab3381be7edd9a7fa75bbff2ea44d3614
shurik9903 <59256714+shurik9903@users.noreply.github.com>
2023-09-18 18:22:40 +0700

    update teaching controller

--- src/main/java/org/example/controller/API/TeachingController.java
+++ src/main/java/org/example/controller/API/TeachingController.java
@@ -25,30 +25,30 @@ public class TeachingController {
         }
     }
 
-//    @GET
-//    @Path("/status/{uuid}")
-//    @Produces("application/json; charset=UTF-8")
-//    @TokenRequired
-//    public Response doGetStatus(@PathParam("uuid") String uuid) {
-//        try {
-//            return analysis.getAnalysisStatus(uuid);
-//        } catch (Exception e) {
-//            System.out.println("|Ошибка: " + e);
-//            return Response.status(Response.Status.BAD_REQUEST).entity("|Ошибка: " + e.getMessage()).build();
-//        }
-//    }
-//
-//    @GET
-//    @Path("/result/{uuid}")
-//    @Produces("application/json; charset=UTF-8")
-//    @TokenRequired
-//    public Response doGetResult(@PathParam("uuid") String uuid, @HeaderParam("login") String userLogin) {
-//        try {
-//            return analysis.getAnalysisResult(uuid, userLogin);
-//        } catch (Exception e) {
-//            System.out.println("|Ошибка: " + e);
-//            return Response.status(Response.Status.BAD_REQUEST).entity("|Ошибка: " + e.getMessage()).build();
-//        }
-//    }
+    @GET
+    @Path("/status/{uuid}")
+    @Produces("application/json; charset=UTF-8")
+    @TokenRequired
+    public Response doGetStatus(@PathParam("uuid") String uuid) {
+        try {
+            return teaching.getTeachingStatus(uuid);
+        } catch (Exception e) {
+            System.out.println("|Ошибка: " + e);
+            return Response.status(Response.Status.BAD_REQUEST).entity("|Ошибка: " + e.getMessage()).build();
+        }
+    }
+
+    @GET
+    @Path("/result/{uuid}")
+    @Produces("application/json; charset=UTF-8")
+    @TokenRequired
+    public Response doGetResult(@PathParam("uuid") String uuid, @HeaderParam("login") String userLogin) {
+        try {
+            return teaching.getTeachingResult(uuid, userLogin);
+        } catch (Exception e) {
+            System.out.println("|Ошибка: " + e);
+            return Response.status(Response.Status.BAD_REQUEST).entity("|Ошибка: " + e.getMessage()).build();
+        }
+    }
 
 }
